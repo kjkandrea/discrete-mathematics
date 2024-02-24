1778269039e25a292a49d1752bbcf3283315aedc
kjkandrea <kjkandrea@gmail.com>
2024-02-24 18:29:21 +0900

    refactor: 변수 초기화를 위해 함수 스코프 개선

--- 01-03_flow-chart-to-code.js
+++ 01-03_flow-chart-to-code.js
@@ -1,14 +1,18 @@
-let M = 1;
-let F = 1;
-
 function factorial(N) {
-  F = F * M
-  if (M === N) {
-    console.log(F);
-    return;
-  }
-  M += 1;
-  factorial(N);
+  let M = 1;
+  let F = 1;
+
+  (function recur() {
+    F = F * M;
+
+    if (M === N) {
+      console.log(F);
+      return;
+    }
+    M += 1;
+    recur(N);
+  }());
+
 }
 
 factorial(6) // 720
